1f2501cce3aca7c193e5af26263cc0bcc0e3f1fb
John Su <Johnchangsu@gmail.com>
2024-04-27 13:14:32 +1000

    Added time sampling

--- src/torch_DE/utils/data_handler.py
+++ src/torch_DE/utils/data_handler.py
@@ -1,5 +1,6 @@
 
 import torch
+from torch_DE.utils.sampling import add_random_time,add_random_time_point
 class Data_handler(dict):
     def __init__(self):
         super().__init__()
@@ -27,3 +28,36 @@ class Data_handler(dict):
 
         self.device = device
 
+    def add_time(self,*,dim = -1,ignore_groups = None):
+        #Ignored groups do not get resampled when time_resample is called!
+        self.time_states ={}
+
+        if isinstance(ignore_groups,str):
+            ignore_groups = [ignore_groups]
+
+        for key in self.keys():
+            if key not in ignore_groups:
+                tensor = self[key]
+                self[key] =torch.cat([tensor,torch.zeros((tensor.shape[0],1),device=tensor.device)],dim = dim)
+                self.time_states[key] = 'all'
+
+    def set_time_sampling(self,*keys,sampling_method = 'all'):
+        '''
+        Set the sampling method for different groups.
+
+        sampling method:
+            - 'all': all time points are sampled randomly along the interval
+            - 'point': A single random time point from the interval is assigned to the group
+        '''
+        if (sampling_method != 'all') and (sampling_method != 'point'):
+            raise ValueError(f'sampling_method takes the strings of "all" or "point"')
+        
+        for key in keys:
+            self.time_states[key] = sampling_method
+
+    def time_resample(self,interval):
+        for key in self.time_states.keys():
+            if self.time_states[key] == 'all':
+                add_random_time(self[key],interval)
+            else:
+                add_random_time_point(self[key],interval)
